6ca01d96220f5b59afc53400b0e924b598fc54cc
Milad Khnefes <milad.khnefes.16@gmail.com>
2025-08-17 22:13:25 +0300

    Resume Feedback Components

--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -91,7 +91,7 @@ const Resume = () => {
 
                 {feedback ? (
                     <div className="flex flex-col gap-8 animate-in fade-in duration-1000">
-                        <Summary feedback={} />
+                        <Summary feedback={feedback} />
                         <ATS score={feedback.ATS.score || 0} suggestions={feedback.ATS.tips|| []} />
                         <Details feedback={feedback} />
                     </div>
